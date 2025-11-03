9222a84663fed255b6ef2ad70a37ef24a0311411
kwonjeong <kwonjeong9470@gmail.com>
2025-11-03 20:57:35 +0900

    feat(App): 로또 번호 랜덤 발행하는 비공개 메서드 issueTickets 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { amountInput } from "./io/inputHandler.js";
+import { printError, printNumber } from "./io/outputHandler.js";
 import { validationAmount } from "./lotto/Validator.js";
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "./lotto/Lotto.js";
 
 class App {
   async run() {
-    const amountLine = amountInput();
-    const amount = validationAmount(amountLine);
-    const ticketCount = amount / 1000;
+    try {
+      const amountLine = await amountInput();
+      const amount = validationAmount(amountLine);
+      const tickets = this.#issueTickets(amount);
+    } catch (error) {
+      printError(error);
+    }
+  }
+
+  #issueTickets(money) {
+    const count = money / 1000;
+    const make = () => {
+      const nums = Random.pickUniqueNumbersInRange(1, 45, 6);
+      return new Lotto(nums);
+    };
+    return Array.from({ length: count }, make);
   }
 }
 
